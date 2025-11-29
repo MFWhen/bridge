3c38853de27753374914bec5812e95d0168a6cd7
MFWhen <yahia.benmahfoudh@medtech.tn>
2025-11-30 00:21:34 +0100

    .

---
+++ backend/controllers/expensesController.js
@@ -0,0 +1,60 @@
+import expenses from "../routes/expenses";
+
+export const getExpenses = async(req , res) => {
+    try{
+        const expenses = await Expense.find({ user: req.user.id }).sort({ date: -1 });
+
+        res.status(200).json({
+            success: true,
+            count: expenses.length,
+            data: expenses
+            })
+    } catch(error){
+        res.status(500).json({
+            success: false,
+            message: "Server error"
+        })
+    }
+}
+
+export const createExpense = async(req , res) => {
+    try{
+        req.body.user = req.user.id
+
+        const expense = await Expense.create(req.body)
+
+        res.status(201).json({
+            success: true,
+            data: expense
+        })
+
+    } catch(error){
+         if (error.name === 'ValidationError') {
+            const messages = Object.values(error.errors).map(val => val.message);
+            return res.status(400).json({
+            success: false,
+            message: messages.join(', ')
+      });
+    }
+        res.status(500).json({
+            success: false,
+            message: "Server error"
+        })
+    }
+}
+
+export const updateExpense = async(req , res) => {
+    try{
+
+    } catch(error){
+
+    }
+}
+
+export const deleteExpense = async(req , res) => {
+    try{
+
+    } catch(error){
+
+    }
+}
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,21 @@
-import mongoose, { mongo } from "mongoose";
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: String,
+    required: false,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-const userSchema = new mongoose.Schema({
-    
-})
+const User = mongoose.model("User", userSchema);
+module.exports = User;
